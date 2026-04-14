a043a2c32fd6827a351108985a979fd6bf2cf254
CSeongWoo <greentjddn@gmail.com>
2026-04-14 10:43:54 +0900

    refactor 로그추가

--- src/main/java/dripnote/user/service/UserService.java
+++ src/main/java/dripnote/user/service/UserService.java
@@ -28,9 +28,13 @@ public class UserService {
 
     public void logout(String accessToken) {
         String userId = jwtTokenProvider.getSubject(accessToken);
+
+        log.info("회원 로그아웃 요청. userId: {}", userId);
+
         redisService.deleteRefreshToken(userId);
         long expiration = jwtTokenProvider.getExpirationToken(accessToken);
         redisService.setBlackList(accessToken, "logout", Duration.ofMillis(expiration));
+        log.info("로그아웃 완료. userId: {}", userId);
     }
 
     public TokenResponse refresh(String refreshToken) {
@@ -39,6 +43,9 @@ public class UserService {
         }
 
         String userId = jwtTokenProvider.getSubject(refreshToken);
+
+        log.info("회원 refresh 요청. userId: {}", userId);
+
         String savedUserRefreshToken = redisService.getRefreshToken(userId);
         if (savedUserRefreshToken == null || !savedUserRefreshToken.equals(refreshToken)) {
             throw new CustomException(ErrorCode.REFRESH_TOKEN_MISMATCH);
@@ -48,12 +55,16 @@ public class UserService {
 
         TokenResponse newTokenResponse = jwtTokenProvider.createTokenSet(user);
         redisService.setRefreshToken(userId, newTokenResponse.refreshToken());
+        log.info("회원 refresh 완료. userId: {}", userId);
         return newTokenResponse;
     }
 
 
     public void deleteUser(String accessToken) {
         String userId = jwtTokenProvider.getSubject(accessToken);
+
+        log.info("회원탈퇴 요청. userId: {}", userId);
+
         User user = userRepository.getUserByUserId(Long.parseLong(userId))
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
         userRepository.delete(user);
@@ -61,10 +72,14 @@ public class UserService {
 
         long expiration = jwtTokenProvider.getExpirationToken(accessToken);
         redisService.setBlackList(accessToken, "delete", Duration.ofMillis(expiration));
+
+        log.info("회원 탈퇴 완료. userId: {}", userId);
     }
 
     public boolean updateUser(String accessToken, UserUpdateRequest updateRequest) {
         String userId = jwtTokenProvider.getSubject(accessToken);
+        log.info("회원 정보 수정 요청. userId: {}", userId);
+
         Optional<User> userOptional = userRepository.getUserByUserId(Long.parseLong(userId));
         if (userOptional.isEmpty()) {
 
@@ -74,6 +89,8 @@ public class UserService {
         User user = userOptional.get();
         if (StringUtils.hasText(updateRequest.nickname())) {
             user.updateNickname(updateRequest.nickname());
+            log.info("회원 정보 수정 완료. userId: {}", userId);
+            log.info("수정된 닉네임: {}", updateRequest.nickname());
             return true;
         }
         return false;
