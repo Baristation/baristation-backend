e0deff43768cbecbaa6c1329c60be9aaa0903ff2
hyeongdongE <le867142@gmail.com>
2026-04-29 14:15:19 +0900

    fix: OAuth targetUrl 수정

--- src/main/java/baristation/security/handler/OAuth2SuccessHandler.java
+++ src/main/java/baristation/security/handler/OAuth2SuccessHandler.java
@@ -65,9 +65,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         TokenResponse tokenResponse = jwtTokenProvider.createTokenSet(user);
         redisService.setRefreshToken(String.valueOf(user.getUserId()), tokenResponse.refreshToken());
         // 리액트로 보낼 url 생성
-        String targetUrl = UriComponentsBuilder.fromUriString("https://localhost:3000/main")
-                .queryParam("accessToken", tokenResponse.accessToken())
-                .queryParam("refreshToken", tokenResponse.refreshToken())
+        String targetUrl = UriComponentsBuilder.fromUriString("https://dripnote-frontend-web.vercel.app/")
                 .build().toUriString();
 
         log.info("로그인 성공! JWT 발급 완료. targetUrl: {}", targetUrl);
