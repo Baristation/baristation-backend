5a70aee0c3d15eb4e711f7c5991b26f573a49b4b
Choi SeongWoo <149999133+CSeongWoo@users.noreply.github.com>
2026-05-05 18:38:20 +0900

    [fix] main 페이지 DTO 수정
    [fix] main 페이지 DTO 수정

--- src/main/java/baristation/home/payload/response/HomeFlavorResponse.java
+++ src/main/java/baristation/home/payload/response/HomeFlavorResponse.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import baristation.bean.domain.FlavorNote;
 
 public record HomeFlavorResponse(
+        @JsonProperty("flavor_id")
+        Long flavorId,
+
         @JsonProperty("flavor_name")
         String flavorName,
 
@@ -15,6 +18,7 @@ public record HomeFlavorResponse(
 ) {
     public static HomeFlavorResponse from(FlavorNote flavorNote) {
         return new HomeFlavorResponse(
+                flavorNote.getFlavorNoteId(),
                 flavorNote.getNameKo(),
                 flavorNote.getFlavorImageUrl(),
                 "/api/products?flavorId=" + flavorNote.getFlavorNoteId()
